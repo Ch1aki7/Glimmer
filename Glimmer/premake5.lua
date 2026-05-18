1adee84938011e01e78f1fb25ac526a9521695c3
zaphkiel <zaphkielbinah3@gmail.com>
2026-05-18 14:49:19 +0800

    重构premake5.lua结构

---
+++ Glimmer/premake5.lua
@@ -0,0 +1,52 @@
+project "Glimmer"
+    location "."
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("../bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
+
+    pchheader "glpch.h"
+    pchsource "src/glpch.cpp"
+
+    files {
+        "src/**.h",
+        "src/**.cpp",
+        "vendor/glm/glm/**.hpp",
+        "vendor/glm/glm/**.inl",
+        "vendor/stb_image/**.h",
+        "vendor/stb_image/**.cpp",
+        "vendor/tinyobjloader/tiny_obj_loader.h",
+        "vendor/tinyobjloader/tiny_obj_loader.cpp"
+    }
+
+    includedirs {
+        "src",
+        "vendor/spdlog/include",
+        "vendor/GLFW/include",
+        "vendor/Glad/include",
+        "vendor/imgui",
+        "vendor/imgui/backends",
+        "vendor/glm",
+        "vendor/stb_image",
+        "vendor/tinyobjloader",
+        "vendor/entt/src"
+    }
+
+    links {
+        "GLFW",
+        "Glad",
+        "ImGui",
+        "opengl32.lib"
+    }
+
+    filter "system:windows"
+        buildoptions { "/utf-8" }
+        systemversion "latest"
+
+        defines {
+            "GL_PLATFORM_WINDOWS",
+            "GL_BUILD_DLL"
+        }
--- premake5.lua
+++ premake5.lua
@@ -2,24 +2,38 @@ workspace "GlimmerEngine"
     architecture "x64"
     startproject "Sandbox"
 
-    configurations { "Debug", "Release", "Dist" }
+    configurations {
+        "Debug",
+        "Release",
+        "Dist"
+    }
 
-        -- 针对 Debug 配置，定义 GL_DEBUG 宏
-    filter "configurations:Debug"
-        defines "GL_DEBUG"
-        symbols "On" 
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-    -- 针对 Release 配置
-    filter "configurations:Release"
-        defines "GL_RELEASE"
-        optimize "On" 
+filter "configurations:Debug"
+    defines "GL_DEBUG"
+    symbols "On"
 
-    -- 针对 Dist (最终发布) 配置
-    filter "configurations:Dist"
-        defines "GL_DIST"
-        optimize "On"
+filter "configurations:Release"
+    defines "GL_RELEASE"
+    optimize "On"
 
-    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+filter "configurations:Dist"
+    defines "GL_DIST"
+    optimize "On"
+
+filter {}
+
+IncludeDir = {}
+
+IncludeDir["spdlog"] = "Glimmer/vendor/spdlog/include"
+IncludeDir["GLFW"] = "Glimmer/vendor/GLFW/include"
+IncludeDir["Glad"] = "Glimmer/vendor/Glad/include"
+IncludeDir["ImGui"] = "Glimmer/vendor/imgui"
+IncludeDir["glm"] = "Glimmer/vendor/glm"
+IncludeDir["stb_image"] = "Glimmer/vendor/stb_image"
+IncludeDir["tinyobjloader"] = "Glimmer/vendor/tinyobjloader"
+IncludeDir["entt"] = "Glimmer/vendor/entt/src"
 
 group "Dependencies"
 include "Glimmer/vendor/GLFW"
@@ -27,123 +41,7 @@ include "Glimmer/vendor/Glad"
 include "Glimmer/vendor/imgui"
 group ""
 
-project "Glimmer"
-    location "Glimmer"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "on"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    pchheader "glpch.h" -- 告诉编译器 PCH 的名字
-    pchsource "Glimmer/src/glpch.cpp" -- 只有 Glimmer 项目需要这个源文件
-
-    files {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
-		"%{prj.name}/vendor/glm/glm/**.hpp",
-		"%{prj.name}/vendor/glm/glm/**.inl",
-        "%{prj.name}/vendor/stb_image/**.h",
-		"%{prj.name}/vendor/stb_image/**.cpp",
-        "%{prj.name}/vendor/tinyobjloader/tiny_obj_loader.h",
-        "%{prj.name}/vendor/tinyobjloader/tiny_obj_loader.cpp"
-    }
-    
-    includedirs {
-        "%{prj.name}/src",
-        "%{prj.name}/vendor/spdlog/include",
-        "%{prj.name}/vendor/GLFW/include",
-        "%{prj.name}/vendor/Glad/include",
-        "%{prj.name}/vendor/imgui",
-        "%{prj.name}/vendor/imgui/backends",
-        "%{prj.name}/vendor/glm",
-        "%{prj.name}/vendor/stb_image",
-        "%{prj.name}/vendor/tinyobjloader",
-        "%{prj.name}/vendor/entt/src"
-    }
-
-    links {
-        "GLFW", -- 直接写项目名，Premake 会自动处理静态库链接
-        "opengl32.lib",
-        "Glad",
-        "ImGui"
-    }
-
-    filter "system:windows"
-    buildoptions { "/utf-8" }
-    systemversion "latest"
-    defines {
-        "GL_PLATFORM_WINDOWS",
-        "GL_BUILD_DLL" -- 预留，虽然我们现在是静态库
-    }
-
-project "Sandbox"
-    location "Sandbox"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "on"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
-    }
-
-    includedirs {
-        "Glimmer/src", -- 沙盒需要引用引擎的代码
-        "Glimmer/vendor/spdlog/include",
-        "Glimmer/vendor/imgui",
-        "Glimmer/vendor/glm",
-        "Glimmer/vendor/entt/src"
-    }
-
-    links {
-        "Glimmer" -- 沙盒链接引擎的静态库
-    }
-
-    filter "system:windows"
-    buildoptions { "/utf-8" }
-    systemversion "latest"
-    defines {
-        "GL_PLATFORM_WINDOWS"
-    }
-
-    
-project "GlimmerEditor"
-    location "GlimmerEditor"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "on"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
-    }
-
-    includedirs {
-        "Glimmer/src",
-        "Glimmer/vendor/spdlog/include",
-        "Glimmer/vendor/imgui",
-        "Glimmer/vendor/glm",
-        "Glimmer/vendor/entt/src"
-    }
-
-    links {
-        "Glimmer"
-    }
-
-    filter "system:windows"
-    buildoptions { "/utf-8" }
-    systemversion "latest"
-    defines {
-        "GL_PLATFORM_WINDOWS"
-    }
+include "Glimmer"
+include "Sandbox"
+include "GlimmerEditor"
+include "CGCourseProject"
